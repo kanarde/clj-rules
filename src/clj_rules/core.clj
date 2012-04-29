4ce37a8096f3ae038cefe51d2b50cc59a65b186b
Michal Miller <kanarde@gmail.com>
2012-04-29 02:43:18 -0400

    Organized rules; added ordinal rule ordering

--- src/clj_rules/core.clj
+++ src/clj_rules/core.clj
@@ -1,17 +1,32 @@
 (ns clj-rules.core)
 
-(def rule1 {:rule #(> (get-in % [:person :age] -1) 21)
-            :action #(assoc-in % [:policy :agebump] true)})
+; Classic "insurance" rules to play with
+(def rules
+  [ {:name "Increase cost by 20% for ages 21 to 25"
+     :ordinal 2
+     :rule #(let [{{age :age} :person} %] (and (>= age 21) (<= age 25)) )
+     :action (fn [s] (update-in s [:price] #(* % 1.20) ))}
+    
+    {:name "Increase cost by 5% for males"
+     :ordinal 3
+     :rule #(let [{{sex :sex} :person} %] (= sex :male) )
+     :action (fn [s] (update-in s [:price] #(* % 1.05) ))}
 
-(def rule2 {:rule #(< (get-in % [:person :age] -1) 25)
-            :action #(assoc-in % [:policy :agebump2] true)})
+    {:name "Increase cost by $40 for red car"
+     :ordinal 1
+     :rule #(let [{{color :color} :car} %] (= color :red) )
+     :action (fn [s] (update-in s [:price] #(+ % 40) ))}
+    ])
 
-(def rule3 {:rule #(get-in % [:policy :agebump] false)
-            :action #(update-in % [:price] (fn [x] (+ x 10)))})
-
-(def rules [rule3 rule2 rule1])
-
-(def state {:person {:name "Bob" :age 22} :price 5})
+; Customer data
+(def customer {:person {:name "Bob"
+                     :age 22
+                     :sex :male}
+            :car {:make "Honda"
+                  :model "Civic"
+                  :year 2000
+                  :color :red}
+            :price 439.99})
 
 ; Naive Approach #1: Single Pass Match & Execute
 (defn run-rules [rules state]
@@ -20,9 +35,10 @@
 ; Niave Approach #2: Muli-Pass Match & Execute with no rule repeats
 (defn rule-eng [rules state]
   (let [{rules-to-run true rules-to-skip false} (group-by #((:rule %) state) rules)
-        result ((apply comp (map #(:action %) rules-to-run)) state)]
-        (if (and (not (identical? state result)) (not-empty rules-to-skip))
-          (recur rules-to-skip result)
-          result)))
+        sorted-rules-to-run (reverse (sort-by :ordinal rules-to-run))
+        result ((apply comp (map #(:action %) sorted-rules-to-run)) state)]
+    (if (and (not (identical? state result)) (not-empty rules-to-skip))
+      (recur rules-to-skip result)
+      result)))
 
-(rule-eng rules state)
+(rule-eng rules customer)
